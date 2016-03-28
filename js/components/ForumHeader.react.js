31a33a1982be3efe902ade9e8486d370547ea846
JeremyCarlsten <jcc.993@gmail.com>
2016-03-28 15:14:56 +0100

    Adding JSX

--- js/components/ForumHeader.react.js
+++ js/components/ForumHeader.react.js
@@ -4,17 +4,31 @@ var ForumHeader = React.createClass({
 
         console.log(this.props.allAnswers);
 
-        return React.createElement(
-            'nav', {className: 'navbar navbar-default'},
-            React.createElement('div', {className: 'container-fluid'},
-                React.createElement('div', {className: 'navbar-header'},
-                    React.createElement('a',
-                        {
-                            className: 'navbar-brand',
-                            href: '#'
-                        }, "React Forum")
-                )
-            )
+
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a href="#" className="navbar-brand">
+                            React Forum
+                        </a>
+                    </div>
+                </div>
+            </nav>
         );
+
+        // Longhand
+        //return React.createElement(
+        //    'nav', {className: 'navbar navbar-default'},
+        //    React.createElement('div', {className: 'container-fluid'},
+        //        React.createElement('div', {className: 'navbar-header'},
+        //            React.createElement('a',
+        //                {
+        //                    className: 'navbar-brand',
+        //                    href: '#'
+        //                }, "React Forum")
+        //        )
+        //    )
+        //);
     }
 });
